72b765679c4642b1bce3ccea37530d339296fc72
zzhou7 <zzhou.be07@nycu.edu.tw>
2022-06-06 17:07:58 +0800

    Software-algorithm workable function backup

--- Plugins/org.mitk.lancet.spinectregistration/src/internal/SpineCTRegistration.cpp
+++ Plugins/org.mitk.lancet.spinectregistration/src/internal/SpineCTRegistration.cpp
@@ -35,31 +35,6 @@ found in the LICENSE file.
 #include <vtkSphereSource.h>
 #include <ep\include\vtk-9.1\vtkTransformFilter.h>
 
-// // Reset the origin of an mitk::Image to (0, 0, 0), realign the image's axes to the standard xyz axes
-// void SpineCTRegistration::ResetImage()
-// {
-//   // Image origin to (0, 0, 0)
-//   auto inputImage = dynamic_cast<mitk::Image *>(m_CtImageDataNode->GetData());
-//   
-//   mitk::Point3D imageOrigin;
-//   imageOrigin[0] = 0.0;
-//   imageOrigin[1] = 0.0;
-//   imageOrigin[2] = 0.0;
-//   
-//   inputImage->SetOrigin(imageOrigin);
-//   
-//   // Align the image's axes to the standard xyz axes
-//   auto tmpVtkTransform = vtkSmartPointer<vtkTransform>::New();
-//   vtkSmartPointer<vtkMatrix4x4> tmpVtkMatrix = vtkSmartPointer<vtkMatrix4x4>::New();
-//   tmpVtkTransform->Identity();
-//   tmpVtkTransform->GetMatrix(tmpVtkMatrix);
-//   
-//   inputImage->GetGeometry(0)->SetIndexToWorldTransformByVtkMatrixWithoutChangingSpacing(tmpVtkMatrix);
-//   // SetIndexToWorldTransformByVtkMatrix(tmpVtkMatrix) will set the spacing as (1, 1, 1),
-//   // because the spacing is determined by the matrix diagonal.
-// 	// So SetIndexToWorldTransformByVtkMatrixWithoutChangingSpacing(tmpVtkMatrix) which keep the spacing regardless of the
-// 	// input matrix
-// }
 
 void SpineCTRegistration::ReconstructSpineSurface()
 {
@@ -110,9 +85,9 @@ void SpineCTRegistration::DEMOregistration()
 	std::ostringstream os;
 	landmarkRegistrator->GetResult()->Print(os);
 
-	m_Controls.textBrowser_DEMOigt->append("-------------Start landmark registration----------");
-	m_Controls.textBrowser_DEMOigt->append(QString::fromStdString(os.str()));
-	m_Controls.textBrowser_DEMOigt->append("-------------End landmark registration----------");
+	m_Controls.textBrowser_Registration->append("-------------Start landmark registration----------");
+	m_Controls.textBrowser_Registration->append(QString::fromStdString(os.str()));
+	m_Controls.textBrowser_Registration->append("-------------End landmark registration----------");
 
 // m_G_patientCT_CTdrf_matrix = landmarkRegistrator->GetResult()->GetData();
 
@@ -163,9 +138,9 @@ void SpineCTRegistration::DEMOregistration()
 // 	std::ostringstream os;
 // 	landmarkRegistrator->GetResult()->Print(os);
 //
-// 	m_Controls.textBrowser_DEMOigt->append("-------------Start landmark registration----------");
-// 	m_Controls.textBrowser_DEMOigt->append(QString::fromStdString(os.str()));
-// 	m_Controls.textBrowser_DEMOigt->append("-------------End landmark registration----------");
+// 	m_Controls.textBrowser_Registration->append("-------------Start landmark registration----------");
+// 	m_Controls.textBrowser_Registration->append(QString::fromStdString(os.str()));
+// 	m_Controls.textBrowser_Registration->append("-------------End landmark registration----------");
 //
 // 	// m_G_patientCT_CTdrf_matrix = landmarkRegistrator->GetResult()->GetData();
 //
@@ -189,43 +164,196 @@ void SpineCTRegistration::DEMOregistration()
 // }
 
 
-vtkMatrix4x4* GetMatrixCtDrfToPatientCt(mitk::PointSet* pointset_steelballsInCtDrf, mitk::Image* inputCtImage, double voxelThreshold, double facetNumUpperBound, double facetNumLowerBound)
+double* GetMatrixAnyToolDrfToRenderingWindow(mitk::PointSet* standardSteelballCenters /*from hardware design*/, mitk::Image* inputCtImage /*should contain steelballs*/, double voxelThreshold /*steelball voxel: 3000*/,
+	double facetNumUpperBound /*1440*/, double facetNumLowerBound /*500*/, double arrayNdiCtDrf[16] /*NDI data*/,
+	double arrayNdiTrackingDrf[16] /*NDI data*/)
 {
-	auto pointset_extractedballs = ObtainSteelballCenters(inputCtImage, voxelThreshold, facetNumUpperBound, facetNumLowerBound);
+	// --------------------------Extract steelball centers from the CT image----------------------------
+	// The isosurface of all steelballs into a single polydata
+
+	auto mitkSteelBallSurfaces = mitk::Surface::New();
+	mitk::ImageToSurfaceFilter::Pointer imageToSurfaceFilter = mitk::ImageToSurfaceFilter::New();
+
+	imageToSurfaceFilter->SetInput(inputCtImage);
+	imageToSurfaceFilter->SetThreshold(voxelThreshold);
+	mitkSteelBallSurfaces = imageToSurfaceFilter->GetOutput();
+
+	// Separate steelball surface by examining their connectivity
+	vtkNew<vtkConnectivityFilter> vtkConnectivityFilter;
+	vtkConnectivityFilter->SetInputData(mitkSteelBallSurfaces->GetVtkPolyData());
+
+	vtkConnectivityFilter->SetExtractionModeToAllRegions();
+	vtkConnectivityFilter->Update();
+	int numberOfPotentialSteelBalls = vtkConnectivityFilter->GetNumberOfExtractedRegions();
+
+
+	auto mitkSingleSteelballCenterPointset = mitk::PointSet::New(); // store each steelball's center
+	double centerOfAllSteelballs[3]{ 0, 0, 0 };                       // the center of all steel balls
+
+	// Iterate over all the potential steelball regions, extract the real ones by assessing their size (facets numbers)
+	vtkConnectivityFilter->SetExtractionModeToSpecifiedRegions();
+	for (int m = 0; m < numberOfPotentialSteelBalls; m++)
+	{
+		vtkConnectivityFilter->InitializeSpecifiedRegionList();
+		vtkConnectivityFilter->AddSpecifiedRegion(m);
+		vtkConnectivityFilter->Update();
+
+		auto vtkSingleSteelBallSurface = vtkConnectivityFilter->GetPolyDataOutput();
+
+		auto numberOfCells =
+			vtkSingleSteelBallSurface->GetNumberOfCells(); // the total number of cells of a single mesh surface; each cell
+														   // stores one facet of the mesh surface
+
+
+
+		if (numberOfCells < facetNumUpperBound && numberOfCells > facetNumLowerBound)
+		{
+			std::vector<double> inp_x(
+				numberOfCells); // inp_x, inp_y and inp_z store one point of each facet on the mesh surface
+			std::vector<double> inp_y(
+				numberOfCells); // inp_x, inp_y and inp_z store one point of each facet on the mesh surface
+			std::vector<double> inp_z(
+				numberOfCells); // inp_x, inp_y and inp_z store one point of each facet on the mesh surface
+
+			for (int n = 0; n < numberOfCells; n++)
+			{
+				auto tmpPoint = vtkSingleSteelBallSurface->GetCell(n)->GetPoints()->GetPoint(0);
+
+				inp_x[n] = tmpPoint[0];
+				inp_y[n] = tmpPoint[1];
+				inp_z[n] = tmpPoint[2];
+			}
+
+			// use inp_x, inp_y and inp_z to simulate a sphere
+			double cx, cy, cz;
+			double R;
+
+			lancetAlgorithm::fit_sphere(inp_x, inp_y, inp_z, cx, cy, cz, R);
+
+			mitk::Point3D mitkTmpCenterPoint3D;
+			mitkTmpCenterPoint3D[0] = cx;
+			mitkTmpCenterPoint3D[1] = cy;
+			mitkTmpCenterPoint3D[2] = cz;
+			mitkSingleSteelballCenterPointset->InsertPoint(mitkTmpCenterPoint3D);
+
+			centerOfAllSteelballs[0] = centerOfAllSteelballs[0] + cx;
+			centerOfAllSteelballs[1] = centerOfAllSteelballs[1] + cy;
+			centerOfAllSteelballs[2] = centerOfAllSteelballs[2] + cz;
+
+		}
+
+
+	}
+
+	int numberOfActualSteelballs = mitkSingleSteelballCenterPointset->GetSize();
+
+	centerOfAllSteelballs[0] = centerOfAllSteelballs[0] / numberOfActualSteelballs;
+	centerOfAllSteelballs[1] = centerOfAllSteelballs[1] / numberOfActualSteelballs;
+	centerOfAllSteelballs[2] = centerOfAllSteelballs[2] / numberOfActualSteelballs;
+
+	// Sort the centers of the separate steelballs according to their distances to the group center
+	std::vector<double> distancesToPointSetCenter(numberOfActualSteelballs);
+	std::vector<int> distanceRanks(numberOfActualSteelballs);
+
+	for (int i = 0; i < numberOfActualSteelballs; i++)
+	{
+		distancesToPointSetCenter[i] =
+			sqrt(pow(centerOfAllSteelballs[0] - mitkSingleSteelballCenterPointset->GetPoint(i)[0], 2) +
+				pow(centerOfAllSteelballs[1] - mitkSingleSteelballCenterPointset->GetPoint(i)[1], 2) +
+				pow(centerOfAllSteelballs[2] - mitkSingleSteelballCenterPointset->GetPoint(i)[2], 2));
+
+		distanceRanks[i] = i;
+	}
+
+
+	for (int i = 0; i < numberOfActualSteelballs - 2; i++)
+	{
+		for (int j = 0; j < numberOfActualSteelballs - 1 - i; j++)
+		{
+			double temp = 0;
+			double temp2 = 0;
+			if (distancesToPointSetCenter[j] > distancesToPointSetCenter[j + 1])
+			{
+				temp = distancesToPointSetCenter[j];
+				distancesToPointSetCenter[j] = distancesToPointSetCenter[j + 1];
+				distancesToPointSetCenter[j + 1] = temp;
+
+				temp2 = distanceRanks[j];
+				distanceRanks[j] = distanceRanks[j + 1];
+				distanceRanks[j + 1] = temp2;
+			}
+		}
+	}
+
+
+
+	auto mitkSortedSingleSteelballCenterPointset = mitk::PointSet::New();
+
+	
+
+	for (int i = 0; i < numberOfActualSteelballs; i++)
+	{
+		mitkSortedSingleSteelballCenterPointset->InsertPoint(mitkSingleSteelballCenterPointset->GetPoint(distanceRanks[i]));
+
+		int m = i;
+	}
+
+	int k = mitkSortedSingleSteelballCenterPointset->GetSize();
+
+
+	//-----------------------------renderingWindow to CtDrf-----------------------------------
 	
 	auto landmarkRegistrator = mitk::SurfaceRegistration::New();
 
-	if (pointset_extractedballs != nullptr && pointset_steelballsInCtDrf != nullptr)
+	if (standardSteelballCenters != nullptr)
 	{
 
-		auto targetPointset = dynamic_cast<mitk::PointSet*>(pointset_extractedballs); // source pointSet needs an extra offset: G_renderWindow_patientCT_matrix
-
-		auto sourcePointset = dynamic_cast<mitk::PointSet*>(pointset_steelballsInCtDrf);
-		landmarkRegistrator->SetLandmarksSrc(sourcePointset);
-		landmarkRegistrator->SetLandmarksTarget(targetPointset);
+		landmarkRegistrator->SetLandmarksSrc(standardSteelballCenters);
+		landmarkRegistrator->SetLandmarksTarget(mitkSortedSingleSteelballCenterPointset);
 		landmarkRegistrator->ComputeLandMarkResult();
 
 	}
 
-	std::ostringstream os;
-	landmarkRegistrator->GetResult()->Print(os);
+
+	vtkMatrix4x4* matrixRenderingWindowToCtDrf = landmarkRegistrator->GetResult();
+
+
+	//------------------------------CtDrf to trackingDrf-------------------------
+	auto toolMatrix = vtkMatrix4x4::New();
+	auto ReferenceMatrix = vtkMatrix4x4::New();
+	toolMatrix->DeepCopy(arrayNdiTrackingDrf);
+	ReferenceMatrix->DeepCopy(arrayNdiCtDrf);
+	
 
 
 
-	auto patientCT_renderWindow_matrix = vtkSmartPointer<vtkMatrix4x4>::New();
-	vtkMatrix4x4::Invert(inputCtImage->GetGeometry()->GetVtkMatrix(), patientCT_renderWindow_matrix);
-	vtkTransform* tmpTrans = vtkTransform::New();
-	tmpTrans->Identity();
+	ReferenceMatrix->Invert();
+	
+
+	auto tmpTransform = vtkTransform::New();
+	tmpTransform->PostMultiply();
+	tmpTransform->Identity();
+	tmpTransform->Concatenate(toolMatrix);
+	tmpTransform->Concatenate(ReferenceMatrix);
+
+	auto matrixCtDrfToTrackingDrf = tmpTransform->GetMatrix();
+
+
+
+	//-----------------Assemble and output----------------
+	auto tmpTrans = vtkTransform::New();
 	tmpTrans->PostMultiply();
-	tmpTrans->SetMatrix(landmarkRegistrator->GetResult());
-	tmpTrans->Concatenate(patientCT_renderWindow_matrix);
-	tmpTrans->Inverse();
-	tmpTrans->Update();
+	tmpTrans->Identity();
+	tmpTrans->Concatenate(matrixCtDrfToTrackingDrf);
+	tmpTrans->Concatenate(matrixRenderingWindowToCtDrf);
 
 
-	vtkSmartPointer<vtkMatrix4x4> CTdrf_patientCT_matrix = vtkSmartPointer<vtkMatrix4x4>::New();
-	vtkMatrix4x4::Invert(tmpTrans->GetMatrix(), CTdrf_patientCT_matrix);
 
-	return tmpTrans->GetMatrix();
+	double* a = tmpTrans->GetMatrix()->GetData();
+
+	auto test = tmpTrans->GetMatrix();
+
+	return tmpTrans->GetMatrix()->GetData();
+
 
 }
