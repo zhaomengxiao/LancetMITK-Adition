81b23f5286583ccddca5058458073cb06ddcdf65
Modify on life <sh4a01@163.com>
2022-10-24 14:37:30 +0800

    Runtime profile of the product
    Update the runtime file (. provisioning) of the product plug-in to be created in the CMake phase.
    This will be the runtime configuration file for the smallest cell.

--- Plugins/org.mitk.lancet.tha.application/files.cmake
+++ Plugins/org.mitk.lancet.tha.application/files.cmake
@@ -38,6 +38,9 @@ set(resources_dir ${CMAKE_CURRENT_SOURCE_DIR}/resources)
 set(resources_target_dir ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/resources)
 execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory ${resources_dir} ${resources_target_dir})
 
+#! Create provisioning file
+include(create_provisioning.cmake)
+
 foreach(file ${SRC_CPP_FILES})
   set(CPP_FILES ${CPP_FILES} src/${file})
 endforeach(file ${SRC_CPP_FILES})
