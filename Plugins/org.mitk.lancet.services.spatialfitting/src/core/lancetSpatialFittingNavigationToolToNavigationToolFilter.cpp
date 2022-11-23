4b5decf56220ce3657347ba71395d1333076f53b
zzhou7 <zzhou.be07@nycu.edu.tw>
2022-11-23 17:21:47 +0800

    Feature(THA2.0 registration): 2 kinds of navigation filters
    SpatialFittingNavagationToolToSpaceFilter: apply a registration matrix to all the incoming NavigationData
    
    SpatialFittingNavigationToolToNavigationToolFilter: display all the other tools realtive to the given tool

--- Plugins/org.mitk.lancet.services.spatialfitting/src/core/lancetSpatialFittingNavigationToolToNavigationToolFilter.cpp
+++ Plugins/org.mitk.lancet.services.spatialfitting/src/core/lancetSpatialFittingNavigationToolToNavigationToolFilter.cpp
@@ -2,35 +2,70 @@
 
 BEGIN_SPATIAL_FITTING_NAMESPACE
 
-struct NavigationToolToNavigationToolFilter::NavigationToolToNavigationToolFilterPrivateImp
-{
+// struct NavigationToolToNavigationToolFilter::NavigationToolToNavigationToolFilterPrivateImp
+// {
+// };
 
-};
+// NavigationToolToNavigationToolFilter::NavigationToolToNavigationToolFilter()
+// 	: imp(std::make_shared<NavigationToolToNavigationToolFilterPrivateImp>())
+// {
+// }
 
-NavigationToolToNavigationToolFilter::NavigationToolToNavigationToolFilter()
-	: imp(std::make_shared<NavigationToolToNavigationToolFilterPrivateImp>())
+NavigationToolToNavigationToolFilter::NavigationToolToNavigationToolFilter() : mitk::NavigationDataToNavigationDataFilter()
 {
-	mitk::NavigationData::Pointer ucsTool;
+	m_UCSNavigationData = mitk::NavigationData::New();
 }
 
 NavigationToolToNavigationToolFilter::~NavigationToolToNavigationToolFilter()
 {
+	m_UCSNavigationData = nullptr;
 }
 
-mitk::NavigationData::Pointer 
-  NavigationToolToNavigationToolFilter::GetUCSTool() const
-{
-	return mitk::NavigationData::Pointer();
-}
-
-void NavigationToolToNavigationToolFilter::SetUCSTool(
-	mitk::NavigationData::Pointer tool)
-{
-}
 
 void NavigationToolToNavigationToolFilter::GenerateData()
 {
+	if (m_UCSNavigationData.IsNull())
+	{
+		itkExceptionMacro("Invalid parameter: m_UCSNavigationData NULL.");
+		return;
+	}
 
+	auto inverseUCSNavigationData = m_UCSNavigationData->GetInverse();
+
+	DataObjectPointerArraySizeType numberOfInputs = this->GetNumberOfInputs();
+
+	if (numberOfInputs == 0)
+		return;
+
+	this->CreateOutputsForAllInputs();
+
+	// generate output
+	for (unsigned int i = 0; i < numberOfInputs; ++i)
+	{
+		const mitk::NavigationData* input = this->GetInput(i);
+		assert(input);
+
+		mitk::NavigationData* output = this->GetOutput(i);
+		assert(output);
+
+		if (input->IsDataValid() == false)
+		{
+			output->SetDataValid(false);
+			continue;
+		}
+		//transform nd from move device to reference device coords
+		mitk::AffineTransform3D::Pointer inputTransform = input->GetAffineTransform3D();
+
+		inputTransform->Compose(inverseUCSNavigationData->GetAffineTransform3D());
+		
+		mitk::NavigationData::Pointer res = mitk::NavigationData::New(inputTransform);
+
+		//copy information to output
+		output->Graft(input); // copy all information from input to output
+		output->SetPosition(res->GetPosition());
+		output->SetOrientation(res->GetOrientation());
+		//output->SetDataValid(input->IsDataValid());
+	}
 }
 
 END_SPATIAL_FITTING_NAMESPACE
